34038894c3625fa3a42f5837dede3b29c0201d12
Aidar Ibatullin <amazing.space.invader@gmail.com>
2021-02-03 03:19:29 +0000

    try to get bonuses from database

--- app/Http/Controllers/Api/v1/ControllerPersonal.php
+++ app/Http/Controllers/Api/v1/ControllerPersonal.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Payments;
-use App\Models\FlocktoryCashbacks;
+use App\Models\FlocktoryCashback;
 use App\Models\Polisies;
 use App\Models\Risks;
 use App\Models\Tarrifs;
