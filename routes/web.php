b989366d95ff47338901ac99a24e148f8f70ee73
Andrey Degtyaruk <hlogeon1@gmail.com>
2021-02-09 16:30:33 +0300

    add activation route

--- routes/web.php
+++ routes/web.php
@@ -74,6 +74,8 @@ Route::group(['prefix' => '/api/v1/'], function () {
 
     Route::get('bonus/offers', [ControllerPersonal::class, 'getBonusList']);
 
+    Route::post('bonus/activate', [ControllerPersonal::class, 'activateBonus']);
+
     Route::post('bonus/favorite', [ControllerPersonal::class, 'favoriteBonus']);
 
     //Получение всех страховок
